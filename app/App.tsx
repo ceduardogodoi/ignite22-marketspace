1b99b71b4a240b916f59a9838683b1119ccfdbfb
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-05-11 23:14:29 -0300

    Adiciona tokens de cor e fonte

--- app/App.tsx
+++ app/App.tsx
@@ -1,26 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { Box, NativeBaseProvider } from 'native-base';
+import { Box, HStack, Heading, NativeBaseProvider, Text } from 'native-base';
+import { Karla_400Regular, Karla_700Bold, useFonts } from '@expo-google-fonts/karla';
 
 import { theme } from './config/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Karla_400Regular,
+    Karla_700Bold,
+  });
+
+  if (!fontsLoaded) return null;
+
   return (
     <NativeBaseProvider theme={theme}>
       <StatusBar style="auto" />
 
-      <View style={styles.container}>
-        <Box backgroundColor="product.blue">Open up App.tsx to start working on your app!</Box>
-      </View>
+      <HStack flex={1} justifyContent="center" alignItems="center">
+        <Box flex={1} justifyContent="center" alignItems="center">
+          <Heading fontSize="sm" color="custom.blue">Heading</Heading>
+          <Text color="custom.red-light">Open up App.tsx to start working on your app!</Text>
+        </Box>
+      </HStack>
     </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- app/config/theme.ts
+++ app/config/theme.ts
@@ -2,10 +2,36 @@ import { extendTheme } from 'native-base';
 
 export const theme = extendTheme({
   colors: {
-    product: {
-      blue: '#364D9D',
+    custom: {
+      'blue': '#364D9D',
+      'blue-light': '#647AC7',
+
+      'gray-1': '#1A181B',
+      'gray-2': '#3E3A40',
+      'gray-3': '#5F5B62',
+      'gray-4': '#9F9BA1',
+      'gray-5': '#D9D8DA',
+      'gray-6': '#EDECEE',
+      'gray-7': '#F7F7F8',
+
+      'red-light': '#EE7979',
     },
-  }
-} as const);
+  },
+
+  fontConfig: {
+    Karla: {
+      700: {
+        normal: 'Karla_700Bold',
+      },
+      400: {
+        normal: 'Karla_400Regular',
+      },
+    },
+  },
+  fonts: {
+    heading: 'Karla',
+    body: 'Karla',
+  },
+});
 
 export type Theme = typeof theme
